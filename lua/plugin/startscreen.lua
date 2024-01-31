406faa1a20b0e21d7f9adb1504a29fc28b15e430
LeoRiether <leonardoalves8217@gmail.com>
2024-01-31 16:30:59 -0300

    alpha.nvim: Clean startscreen up

--- lua/plugin/startscreen.lua
+++ lua/plugin/startscreen.lua
@@ -12,30 +12,30 @@ local function footer()
     -- local plugins = #vim.tbl_keys(packer_plugins)
     local plugins = require("lazy").stats().count
     local v = vim.version()
-    local datetime = os.date " %d-%m-%Y   %H:%M:%S"
     local platform = vim.fn.has "win32" == 1 and "" or ""
-    return string.format(" %d   v%d.%d.%d %s  %s", plugins, v.major, v.minor, v.patch, platform, datetime)
+    return string.format(" %d   v%d.%d.%d %s", plugins, v.major, v.minor, v.patch, platform)
 end
 
 -- header
-local hstr = require("plugin.headers").random()
--- local fd = io.popen(string.format("printf '%s' | lolcat -f", table.concat(hstr, '\\n')))
--- hstr = fd:read('*a')
--- fd:close()
--- local header = {}
--- for line in string.gmatch(hstr, "([^\n]+)") do
---     header[#header+1] = line
--- end
--- dashboard.section.header.val = header
-dashboard.section.header.val = hstr
--- dashboard.section.header.opts.hl = "AlphaCol" .. math.random(5) -- doesn't work anymore?
+dashboard.section.header.val = require("plugin.headers").random()
+
+-- date
+local datetime = os.date " %d-%m-%Y   %H:%M:%S"
+local date = {
+    type = "text",
+    val = datetime,
+    opts = {
+        position = "center",
+        hl = "Number",
+    },
+}
 
 -- buttons
 dashboard.section.buttons.val = {
-    button("SPC n n","  New file", "<cmd>enew<cr>"),
+    button("SPC n n", "  New file", "<cmd>enew<cr>"),
     button("SPC p p", "  Find file", "<cmd>Files<cr>"),
     button("SPC p h", "  Recent files", "<cmd>History<cr>"),
-    button("SPC f f", "  Find word", "<cmd>Rg<cr>"),
+    -- button("SPC f f", "  Find word", "<cmd>Rg<cr>"),
     button("SPC g b", "  Open repo", "<cmd>GBrowse<cr>"),
     -- button("SPC s s", "  Open session", "<cmd>source Session.vim<cr>"),
     button("SPC s s", "  Open session", "<cmd>lua require('persistence').load()<cr>"),
@@ -48,15 +48,26 @@ dashboard.section.footer.val = footer()
 dashboard.section.footer.opts.hl = dashboard.section.header.opts.hl
 
 -- quote
-table.insert(dashboard.config.layout, { type = "padding", val = 1 })
-table.insert(dashboard.config.layout, {
+local quote = {
     type = "text",
-    val = require "alpha.fortune"(),
+    val = require "alpha.fortune" (),
     opts = {
         position = "center",
         hl = "AlphaQuote",
     },
-})
+}
+
+dashboard.config.layout = {
+    { type = "padding", val = 12 },
+    dashboard.section.header,
+    { type = "padding", val = 2 },
+    date,
+    { type = "padding", val = 1 },
+    dashboard.section.footer,
+    { type = "padding", val = 1 },
+    -- dashboard.section.buttons,
+    quote,
+}
 
 require("alpha").setup(dashboard.config)
 
